50146d2eaf6bc63cb5775c732661e2c0878e7125
N-Bee <jeffrey.m.rivera@gmail.com>
2022-02-11 11:29:56 -0500

    remove flask / deps

--- api/main.py
+++ api/main.py
@@ -1,6 +1,3 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from urllib.parse import quote
 import os
 
@@ -13,40 +10,5 @@ credentials = {
     'database'  : os.environ['DB_DATABASE'],
     'port'      : os.environ['DB_PORT'],
 }
-app = Flask(__name__)
-print(credentials)
-# postgresql://username:password@localhost:port/database
 con_string = f"postgresql://{credentials['username']}:%s@{credentials['host']}:5432/{credentials['database']}" % quote('dem0nscr@d00')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = con_string
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-class TestModel(db.Model):
-    __tablename__ = 'matches'
-
-    id = db.Column(db.Integer, primary_key=True)
-    model = db.Column(db.String())
-    price = db.Column(db.Integer())
-
-    def __init__(self, model, doors):
-        self.model = model
-        self.doors = doors
-
-    def __repr__(self):
-        return f"<Test {self.model}>"
-
-@app.route('/test')
-def runtest():
-    v = TestModel.query.all()
-    print(v)
-    return 'OK'
-
-# Test base route 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=7000, debug=True)
