a664ed3cc2c7d25cea1b445047c6cdacce62ab9f
jayonethesk8 <misterjustincox@gmail.com>
2022-02-16 23:07:02 -0500

    * default p1/p2 to AN vs VI * allow nullable date_uploaded column * validate type and url required

--- api/matches/models.py
+++ api/matches/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.forms import ValidationError
 from django.utils import timezone
 
 from . import enums
@@ -25,30 +26,33 @@ from . import enums
 # potential better way of doing enums : https://hackernoon.com/using-enum-as-model-field-choice-in-django-92d8b97aaa63
 class MatchInfo(models.Model):
     # Basic id
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     # Is the content via Fightcade url or Video
     type = models.CharField(
         max_length=3,
         choices=enums.MatchLinkType.choices,
     )
+
     # Fightcade or Video Link (usually youtube)
     url = models.URLField(max_length=200)
+
     # P1/P2 in game character choice
     p1_char = models.CharField(
         max_length=2,
         choices=enums.CharNames.choices,
-        null=True
+        default=enums.CharNames.AN
     )
     p2_char = models.CharField(
         max_length=2,
         choices=enums.CharNames.choices,
-        null=True
+        default=enums.CharNames.VI
     )
     
     winning_char = models.CharField(
         max_length=2,
         choices=enums.CharNames.choices,
-        null=True
+        default=enums.CharNames.VI
     )
 
     # P1/P2 
@@ -58,7 +62,7 @@ class MatchInfo(models.Model):
     # YT info
     timestamp = models.IntegerField(default=0)
     uploader = models.TextField(max_length=100, default='')
-    date_uploaded = models.DateTimeField(default=timezone.now) # timezone.now as a placeholder
+    date_uploaded = models.DateTimeField(null=True)
     video_title = models.TextField(max_length=100, default='')
 
     # db internal
@@ -68,11 +72,11 @@ class MatchInfo(models.Model):
     # added by
     added_by = models.ForeignKey('auth.User', related_name='matches', on_delete=models.CASCADE, null=True)
 
-    # def save(self, *args, **kwargs):
-    #     """
-    #     Save a match~
-    #     """
-    #     super.save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.type or not self.url:
+            raise ValidationError('MatchInfo requires a type/url')
+        super(MatchInfo, self).save(*args, **kwargs)
+             
 
-    def __str__(self):
-        return f'<{self.__name__}> ${self.id} {self.url}'
+    # def __str__(self):
+    #     return f'<{self.__name__}> ${self.id} {self.url}'
